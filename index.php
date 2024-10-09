0dd93e6092a72f95f2a494ce47ca3edb0b6abcf1
Miguel Angel <mmorpar281@g.educaand.es>
2024-10-09 12:31:44 +0200

    Titulo añadido

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
+	<title>Hola mundo</title>
 </head>
 <body>
 <?php echo "<h1>Hello world</h1>"?>
